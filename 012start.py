66506f96cc2c7e388182921f41b75d6d67087f33
Liberbell <liberbell@gmail.com>
2019-08-06 07:59:52 +0900

    012start3

--- 012start.py
+++ 012start.py
@@ -1,1 +1,11 @@
 print(10 < 75)
+print(75 < 10)
+
+if 10 < 75:
+    print('The bigger number is bigger')
+
+kitten = 10
+tiger = 75
+
+if kitten < tiger:
+    print('The kitten weight less than the tiger')
