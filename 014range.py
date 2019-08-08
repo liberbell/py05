0c847fd1788cfe222c4d032708861e1c3f661a35
Liberbell <liberbell@gmail.com>
2019-08-08 10:49:37 +0900

    014range6

--- 014range.py
+++ 014range.py
@@ -3,6 +3,7 @@ numberedContents = range(30)
 print(list(numberedContents))
 
 for c in list(numberedContents):
-    print('Contents: ' + str(c) ' is here')
+    print('Contents: ' + str(c) + ' is here')
 
 luckywinner = range(7, 30, 5)
+print(list(luckywinner))
