73ca3de41fe755c434a63cc9baadc75637c66ab0
Liberbell <liberbell@gmail.com>
2019-08-10 12:14:47 +0900

    017sorted3

--- 017sorted.py
+++ 017sorted.py
@@ -1,3 +1,7 @@
 pointsInGame = [0, -10, 15, -2, 1, 12]
 sortedGame = sorted(pointsInGame)
 print(sortedGame)
+
+children = ['Sue', 'Jerry', 'Linda']
+sortedChildren = sorted(children)
+print(sortedChildren)
