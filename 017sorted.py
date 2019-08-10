f90b13187c6fd85dc92f05d1fa4e5000e4eb641f
Liberbell <liberbell@gmail.com>
2019-08-10 12:24:28 +0900

    017sorted6

--- 017sorted.py
+++ 017sorted.py
@@ -14,3 +14,6 @@ print(sorted(pointsInGame, reverse=True))
 leaderBoard = {231: 'CKL', 123: 'ABC', 432: 'JKC'}
 print(sorted(leaderBoard, reverse=True))
 print(leaderBoard.get(432))
+
+students = [('alice', 'B', 12), ('eliza', 'a', 16), ('tae', 'c', 15)]
+print(sorted(students, key=lambda students:students[0]))
