b1423092f13fdbaf0e23643e5f6c5af227360a85
Liberbell <liberbell@gmail.com>
2019-08-10 12:21:07 +0900

    017sorted6

--- 017sorted.py
+++ 017sorted.py
@@ -10,3 +10,6 @@ print(sorted(['Sue', 'Jerry', 'Linda']))
 
 print(sorted('My favorite child is Linda'.split(), key=str.upper))
 print(sorted(pointsInGame, reverse=True))
+
+leaderBoard = {231: 'CKL', 123: 'ABC', 432: 'JKC'}
+print(sorted(leaderBoard, reverse=True))
