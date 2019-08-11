6d31862c8dab05030b7de666240d12d0a019ef87
Liberbell <liberbell@gmail.com>
2019-08-11 17:15:12 +0900

    018type3

--- 018type.py
+++ 018type.py
@@ -3,3 +3,15 @@ print(type(r))
 print(type(10))
 print(type('a'))
 print(type('Hi there'))
+
+class Car:
+    pass
+
+class Truck(Car):
+    pass
+
+c = Car()
+t = Truck()
+
+print(type(c))
+print(type(t))
