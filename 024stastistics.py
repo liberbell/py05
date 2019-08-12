953b7c9be8e92bccf018ec29b1a03223fef9c2ef
Liberbell <liberbell@gmail.com>
2019-08-13 07:53:08 +0900

    024stastistics7

--- 024stastistics.py
+++ 024stastistics.py
@@ -1,4 +1,5 @@
 import statistics
+import math
 
 agesData = [10, 13, 14, 12, 11, 10, 11, 10, 15]
 print(statistics.mean(agesData))
@@ -9,3 +10,4 @@ print(sorted(agesData))
 
 print(statistics.variance(agesData))
 print(statistics.stdev(agesData))
+print(math.sqrt(statistics.variance(agesData)))
