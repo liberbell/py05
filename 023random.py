8f7756807006d8bd9e30ab8ee2df3dcc859bb701
Liberbell <liberbell@gmail.com>
2019-08-13 07:38:39 +0900

    023random1

--- 023random.py
+++ 023random.py
@@ -7,17 +7,19 @@ print(random.random())
 # else:
 #     print('Tails')
 
-Heads = 0
-Tails = 0
-for i in range(1000):
-    decider = random.randrange(2)
-    # Heads = 0
-    # Tails = 0
-    if decider == 0:
-        print('Heads')
-        Heads += 1
-    else:
-        print('Tails')
-        Tails += 1
+# Heads = 0
+# Tails = 0
+# for i in range(1000):
+#     decider = random.randrange(2)
+#     # Heads = 0
+#     # Tails = 0
+#     if decider == 0:
+#         print('Heads')
+#         Heads += 1
+#     else:
+#         print('Tails')
+#         Tails += 1
+#
+# print('Heads are {0} times, and Tails are {1} times.'.format(Heads, Tails))
 
-print('Heads are {0} times, and Tails are {1} times.'.format(Heads, Tails))
+print(random.randrange(1, 7))
