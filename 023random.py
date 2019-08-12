5d685b7598afd88b0b08bfbf2c05cfdaaaafe606
Liberbell <liberbell@gmail.com>
2019-08-12 10:30:18 +0900

    023random6

--- 023random.py
+++ 023random.py
@@ -7,9 +7,17 @@ if decider == 0:
 else:
     print('Tails')
 
+Heads = 0
+Tails = 0
 for i in range(1000):
     decider = random.randrange(2)
+    # Heads = 0
+    # Tails = 0
     if decider == 0:
         print('Heads')
+        Heads += 1
     else:
         print('Tails')
+        Tails += 1
+
+Print('Heads are {0} times, and Tails are {0} times.'.format(Heads, Tails))
