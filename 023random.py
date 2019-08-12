45a5f95fbb94f93d7ab5cfc50503153ebb41c7ac
Liberbell <liberbell@gmail.com>
2019-08-12 10:28:04 +0900

    023random4

--- 023random.py
+++ 023random.py
@@ -6,3 +6,10 @@ if decider == 0:
     print('Heads')
 else:
     print('Tails')
+
+for i in range(1000):
+    decider = random.randrange(2)
+    if decider == 0:
+        print('Heads')
+    else:
+        print('Tails')
