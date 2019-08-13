12dc7053acf78ee878ad769a35e28dca78c432f3
Liberbell <liberbell@gmail.com>
2019-08-14 07:43:37 +0900

    025iterator5

--- 025iterator.py
+++ 025iterator.py
@@ -2,8 +2,12 @@ import itertools
 
 for x in itertools.count(50, 5):
     print(x)
-    if x == 80:
-        break;
+    if x == 60:
+        break
 
+x = 0
 for c in itertools.cycle('RACECAR'):
     print(c)
+    x += 1
+    if x > 50:
+        break
