b4176feeeea7261c700f25077f6dabd2ef3619a8
Liberbell <liberbell@gmail.com>
2019-08-15 08:00:40 +0900

    031input4

--- 031input.py
+++ 031input.py
@@ -5,3 +5,13 @@ print('Arguments ', sys.argv)
 
 sys.argv.remove(sys.argv[0])
 print('Arguments ', sys.argv)
+
+arguments = sys.argv
+sum = 0
+for arg in arguments:
+    try:
+        number = int(arg)
+        sum += number
+    except Exception:
+        print('Bad input')
+print(sum)
