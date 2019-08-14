bd639ceaf068f25d99e7e58ab544aacb7dcec443
Liberbell <liberbell@gmail.com>
2019-08-15 07:36:53 +0900

    026permutation2

--- 026permutation.py
+++ 026permutation.py
@@ -1,8 +1,8 @@
 import itertools
 
 election = {1: 'Berb', 2: 'Karen', 3: 'Erin'}
-for p in itertools.permutations(election, 2):
+for p in itertools.permutations(election):
     print(p)
 
-for pi in itertools.permutations(election.values):
+for pi in itertools.permutations(election.values()):
     print(p1)
