143520ee5d6ac6f98017a61b57bcbd05ff191bb0
Liberbell <liberbell@gmail.com>
2019-08-16 07:43:37 +0900

    033file3

--- 033file.py
+++ 033file.py
@@ -1,5 +1,7 @@
 
 myFile = open('scores.txt', 'w')
 
-print('Name' + myFile.name)
-print('Mode' + myFile.mode)
+print('Name ' + myFile.name)
+print('Mode ' + myFile.mode)
+
+myFile.write('GBJ: 100\nKHD: 90\nBBB: 89')
