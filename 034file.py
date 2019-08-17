bf485235528006ff8170503a91a5c39f0324a8a6
Liberbell <liberbell@gmail.com>
2019-08-17 11:37:05 +0900

    034file3

--- 034file.py
+++ 034file.py
@@ -2,3 +2,9 @@ myFile = open('scores.txt', 'r')
 
 print('My one Line: ' + myFile.readline())
 myFile.seek(0)
+
+for line in myFile:
+    newHighScore = line.replace('BBB', 'PDJ')
+    print(newHighScore)
+
+myFile.close()
