70301a29648e33e093d0b59712dc196820608076
Liberbell <liberbell@gmail.com>
2019-08-19 07:46:07 +0900

    043caltime4

--- 043caltime.py
+++ 043caltime.py
@@ -6,3 +6,6 @@ testData = now + timedelta(days=2)
 myFirstcorse = now - timedelta(weeks=3)
 print(testData.date())
 print(myFirstcorse.date())
+
+if testData > myFirstcorse:
+    print('Comparison works')
