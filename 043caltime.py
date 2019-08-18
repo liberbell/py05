c7cf9514db88de1798ac07fbd58490344d3e3d43
Liberbell <liberbell@gmail.com>
2019-08-19 07:44:29 +0900

    043caltime2

--- 043caltime.py
+++ 043caltime.py
@@ -1,3 +1,7 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 now = datetime.now()
+
+testData = now + timedelta(days=2)
+myFirstcorse = now - timedelta(weeks=3)
+print(testData.date())
