8e7a1c415cd4396b7934b9fb0a2ef6404c37ce69
Liberbell <liberbell@gmail.com>
2019-08-20 15:07:21 +0900

    045html3

--- 045html.py
+++ 045html.py
@@ -3,3 +3,7 @@ from html.parser import HTMLParser
 class HTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         print('start tag: ', tag)
+        for attr in attrs:
+            print('attr:', attr)
+    def handle_endtag(self, tag):
+        print('End tag:', tag)
