523c1965e2cb231681363c2a629378454934f9e1
Liberbell <liberbell@gmail.com>
2019-08-20 15:08:55 +0900

    045html4

--- 045html.py
+++ 045html.py
@@ -4,6 +4,10 @@ class HTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         print('start tag: ', tag)
         for attr in attrs:
-            print('attr:', attr)
+            print('attr: ', attr)
     def handle_endtag(self, tag):
-        print('End tag:', tag)
+        print('End tag: ', tag)
+    def handle_comment(self, data):
+        print('Comment: ', data)
+    def handle_data(self, data):
+        print('Data: ', data)
