e49361f42fa545fd1a46d2db09c2078d3b6bf53e
Liberbell <liberbell@gmail.com>
2019-08-21 07:48:40 +0900

    046text5

--- 046text.py
+++ 046text.py
@@ -1,10 +1,14 @@
 import textwrap
 
 websitetext = '''
-    Boris Johnson has warned European leaders there is “no prospect of a deal” 
+    Boris Johnson has warned European leaders there is “no prospect of a deal”
     unless they back down amid an intensifying row with Brussels before
     face-to-face talks with Angela Merkel in Berlin tomorrow.
     '''
 
 print('No Dedent:')
 print(textwrap.fill(websitetext))
+
+print('Dedent:')
+dedent_text = textwrap.dedent(websitetext).strip()
+print(dedent_text)
