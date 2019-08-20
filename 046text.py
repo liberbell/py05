05eaac76a406d1c72e40942fbd2e3e612decf936
Liberbell <liberbell@gmail.com>
2019-08-21 07:45:36 +0900

    046text2

---
+++ 046text.py
@@ -0,0 +1,8 @@
+import textwrap
+
+websitetext = '''
+    Boris Johnson has warned European leaders there is “no prospect of a deal”
+    unless they back down amid an intensifying row with Brussels before
+    face-to-face talks with Angela Merkel in Berlin tomorrow.
+    '''
+print('No Dedent:')
