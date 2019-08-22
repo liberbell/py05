cf1fd21ee73c0af522b868a6d2f03b43fc7916cd
Liberbell <liberbell@gmail.com>
2019-08-22 20:34:14 +0900

    047urllib4

--- 047urllib.py
+++ 047urllib.py
@@ -2,7 +2,7 @@ import urllib.request
 import json
 import textwrap
 
-with urllib.request.urlopen('https://www.googleapis.com/books/v1/volumes?q=isbn:1101904224'):
+with urllib.request.urlopen('https://www.googleapis.com/books/v1/volumes?q=isbn:1101904224') as f:
     text = f.read()
     decodedtext = text.decode('utf-8')
     print(decodedtext)
